1041c4716a77efeef573fd3f3a991669abaf0800
Anishatl <66487984+Anishatl@users.noreply.github.com>
2025-05-12 22:39:07 -0400

    Update pose_utils.py

--- pose_utils.py
+++ pose_utils.py
@@ -1,5 +1,22 @@
 import cv2
 import mediapipe as mp
+import numpy as np
+
+def calculate_angle(a, b, c):
+    """Calculate angle between three points in degrees."""
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+
+    ba = a - b
+    bc = c - b
+
+    cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
+    angle = np.arccos(np.clip(cosine_angle, -1.0, 1.0))
+
+    return np.degrees(angle)
+
+
 
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
