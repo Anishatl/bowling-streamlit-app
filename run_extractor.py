701156fca8c32e4f9c146ce0b70c3aff0a8a6186
Anishatl <66487984+Anishatl@users.noreply.github.com>
2025-06-05 20:57:26 -0400

    Update run_extractor.py

--- run_extractor.py
+++ run_extractor.py
@@ -1,7 +1,7 @@
 from pose_angle_extractor import extract_pose_angles_to_csv
 
 if __name__ == "__main__":
-    video_path = "C:\Users\anish\Downloads\Bowling_Training_Data/Mark_Wood_Video.mp4"  # Update this to your local video file path
+    video_path = "C:\Users\anish\Downloads\Bowling_Training_Data\Mark_Wood_Video.mp4"  # Update this to your local video file path
     csv_output_path = "output_angles.csv"
 
     extract_pose_angles_to_csv(video_path, csv_output_path, frame_skip=5)
