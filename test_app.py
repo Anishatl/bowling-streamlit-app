80c4c6b6fbdb68cd0ee4173b1cec2947a575bd6d
Anishatl <66487984+Anishatl@users.noreply.github.com>
2026-01-05 16:05:17 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,4 +1,6 @@
+#Use to test if streamlit is working on not, if this doesn't load see if version is wrong 
 import streamlit as st
 st.title("Simple Streamlit Test")
 st.write("If you see this message, Streamlit is working!")
+
 
